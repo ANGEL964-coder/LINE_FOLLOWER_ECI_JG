fe07235983cc01e9df7c47bf3d6cb0d425ef12b9
ANGEL964-coder <angel.pedreros-a@mail.escuelaing.edu.co>
2026-05-26 13:12:45 -0500

    Update pwm.v

--- src/pwm.v
+++ src/pwm.v
@@ -1,43 +1,38 @@
 module pwm (
-    input  wire              clk,
-    input  wire              rst_n,
-    output reg               Q_motor_der,
-    output reg               Q_motor_izq,
+    input  wire        clk,
+    output reg         Q_motor_der,
+    output reg         Q_motor_izq,
     input  wire signed [7:0] error
 );
 
-    reg [7:0] duty_motor_der;
-    reg [7:0] duty_motor_izq;
-    reg [7:0] cont;
+    reg [7:0] duty_motor_der = 8'b00000000; 
+    reg [7:0] duty_motor_izq = 8'b00000000;
+    reg [7:0] cont = 8'b00000000;
 
     always @(*) begin
-        duty_motor_der = error + 8'd192;
-        duty_motor_izq = 8'd255 - duty_motor_der;
+        duty_motor_der = error + 8'd192; // Duty base de 75%
+        duty_motor_izq = 9'd384 - duty_motor_der;  
+        
     end
 
-    always @(posedge clk or negedge rst_n) begin
+    always @(posedge clk) begin
+        
+        if (cont == 8'b11111111)
+            cont <= 8'b00000000;
+        else
+            cont <= cont + 8'b00000001;
 
-        if(!rst_n) begin
-            cont         <= 8'd0;
-            Q_motor_der  <= 1'b0;
-            Q_motor_izq  <= 1'b0;
-        end
-        else begin
+        // PWM motor izquierdo
+        if (cont < duty_motor_izq)
+            Q_motor_izq <= 1'b1;
+        else
+            Q_motor_izq <= 1'b0;
 
-            cont <= cont + 8'd1;
-
-            // PWM izquierdo
-            if (cont < duty_motor_izq)
-                Q_motor_izq <= 1'b1;
-            else
-                Q_motor_izq <= 1'b0;
-
-            // PWM derecho
-            if (cont < duty_motor_der)
-                Q_motor_der <= 1'b1;
-            else
-                Q_motor_der <= 1'b0;
-        end
+        // PWM motor derecho
+        if (cont < duty_motor_der)
+            Q_motor_der <= 1'b1;
+        else
+            Q_motor_der <= 1'b0;
     end
 
 endmodule
